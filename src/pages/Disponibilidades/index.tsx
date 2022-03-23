2c539449b3c076f4ede38b5d2ce41acddbc28705
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-03-23 15:28:17 -0300

    Trabalhando nas rotas

---
+++ src/pages/Disponibilidades/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Disponbilidades() {
+  return <div>Disponbilidades</div>;
+}
+
+export default Disponbilidades;
---
+++ src/pages/Galeria/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Galeria() {
+  return (
+    <div>
+      <h1>1</h1>
+    </div>
+  );
+}
+
+export default Galeria;
