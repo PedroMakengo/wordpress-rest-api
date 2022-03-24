5128052d8cb6b2e2ae2ba0cf3ab39cb329f88121
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-03-24 06:08:12 -0300

    Terminando a parte de perguntas

---
+++ src/components/ItemQuestion/index.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import styles from "./index.module.css";
+
+import iconeUp from "../../assets/icones/icone-up.svg";
+import iconeDown from "../../assets/icones/icone-down.svg";
+
+interface ItemQuestion {
+  id: number;
+  question: string;
+  response: string;
+}
+
+function ItemQuestion({ id, question, response }: ItemQuestion) {
+  const [itemQuestion, setItemQuestion] = useState(true);
+
+  function handleChange() {
+    setItemQuestion(!itemQuestion);
+    console.log(itemQuestion);
+  }
+  return (
+    <div className={styles.itemQuestion} key={id} onClick={handleChange}>
+      <div className={styles.questionImg}>
+        <h3>{question}</h3>
+        {itemQuestion ? (
+          <img src={iconeDown} alt="Pergunta Fechado" />
+        ) : (
+          <img src={iconeUp} alt="Pergunta aberta" />
+        )}
+      </div>
+      {itemQuestion && <p>{response}</p>}
+    </div>
+  );
+}
+
+export default ItemQuestion;
--- src/components/Questions/index.tsx
+++ src/components/Questions/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import styles from "./index.module.css";
-import ItemQuestion from "./ItemQuestion";
+import ItemQuestion from "../ItemQuestion";
 
 const arrayQuestions = [
   {
